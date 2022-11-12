8c11cc412086d5ea5265d8045fabb037a52b9390
AddOnBethy <110837017+AddOnBethy@users.noreply.github.com>
2022-11-12 01:38:30 +0000

    Add files via upload

---
+++ HealTarget/SpellList.lua
@@ -0,0 +1,30 @@
+SPELLLIST = {
+    DRUID = {
+        "Healing Touch",
+        "Nourish",
+        "Rebirth",
+        "Regrowth",
+        "Revive",
+    },
+    PALADIN = {
+        "Flash of Light",
+        "Holy Light",
+        "Redemption",
+    },
+    PRIEST = {
+        "Binding Heal",
+        "Flash Heal",
+        "Greater Heal",
+        "Heal",
+        "Lesser Heal",
+        "Penance",
+        "Prayer of Healing",
+        "Resurrection",
+    },
+    SHAMAN = {
+        "Ancestral Spirit",
+        "Chain Heal",
+        "Healing Wave",
+        "Lesser Healing Wave",
+    },
+}
